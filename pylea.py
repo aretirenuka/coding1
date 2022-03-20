4a965461f52824239e8a1ea6788011842d9af729
aretirenuka <84584679+aretirenuka@users.noreply.github.com>
2022-03-20 14:34:25 +0530

    Add files via upload

---
+++ pylea.py
@@ -0,0 +1,14 @@
+n=int(input("value of n:"))
+a=int(input("value of a:"))
+c=int(input("value of c:"))
+if n>a:
+    if n>c:
+        print("n is big")
+    else:
+        print("c is big")
+else:
+    if a>c:
+        print("a is big")
+    else:
+        print("c is big")
+         
---
+++ utubeee.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=int(input())
+if(a>b):
+    print("a is big")
+elif(a<b):
+    print("a is small")
+else:
+    print("same")
+    
